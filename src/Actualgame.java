89a7ee5c96c43618948b3d72f873c8184c2984fc
lourdes24590 <62193523+lourdes24590@users.noreply.github.com>
2020-04-04 13:20:06 -0400

    ActualGame with comments

--- src/Actualgame.java
+++ src/Actualgame.java
@@ -6,30 +6,27 @@ public class Actualgame {
 	private BoardTile[] tiles;
 	private final Pane gameroot = new Pane();
 	private Board board = new Board();
-	private int tile_in_row = 15;
-	private int tile_in_col = 15;
-	private int board_w = 800;
+	private int tile_in_row = 15;// board spaces per row
+	private int tile_in_col = 15;// board spaces per col
+	private int board_w = 800;// board size
 	private int board_h = 800;
 	private int tile_w = board_w / tile_in_row;
 	private int tile_h = board_h / tile_in_col;
 	private Rectangle border = new Rectangle(tile_w - 2, tile_h - 2);
 
+	/**
+	 * This gets the actual game screen when you click start running
+	 * 
+	 */
 	public Actualgame() {
-		gameroot.setPrefSize(board_w, board_h);
+		gameroot.setPrefSize(board_w, board_h);// size of the Pane which ends up being the board
 		gameroot.setId("playingpage");
-		// border.setStroke(Color.WHITE);
-		// gameroot.getChildren().add(border);
-		// for (int i = 0; i <= 15; i = i + 15) {
-		// for (int j = 0; j <= 15; j = j + 15) {
-		LetterTilePic lp = new LetterTilePic(LetterTilePic.Letters.A);
+		LetterTilePic lp = new LetterTilePic(LetterTilePic.Letters.A);// this is just a tester
 		Rectangle rect = new Rectangle(tile_w, tile_h);
-		rect.setId("Tiles");
-		lp.getOnDragDetected();
-		// Text text = new Text("hi");
-		gameroot.getChildren().addAll(lp, board);
+		rect.setId("Tiles");// used for css maybe later haven't set up much there yet
+		lp.getOnDragDetected();// doesn't work but i meant for this to make the tiles draggable for user
+		gameroot.getChildren().addAll(lp, board);// this gets these elements onto the screen(pane)
 
-		// }
-		// }
 	}
 
 	public Pane getRootPane() {
