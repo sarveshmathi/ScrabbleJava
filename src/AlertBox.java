ef5b68d1f99fd314ca11ef2158a22c1cafb84ef6
rijalaayush <62912880+rijalaayush@users.noreply.github.com>
2020-04-19 19:04:16 -0400

    Update AlertBox.java

--- src/AlertBox.java
+++ src/AlertBox.java
@@ -185,7 +185,6 @@ public class AlertBox {
 						+ "When you are finished, type the word in the text box.\n\n"
 						+ "Press Confirm Word button to play the word.\n\n"
 						+ "Confirm Word button automatically changes turn.\n\n"
-						+ "Tiles of an incorrect word remain on the board.\n\n"
 						+ "Players decide to end the game at any moment by clicking \"End Game\" button.\n\n"
 						+ "Press Fofriet Turn button to forfeit current player's turn.\n\n"
 						+ "Press Reset button to reset the game.");
