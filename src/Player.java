c1966a0686e72ab0928fb71b42439187d525a136
rijalaayush <62912880+rijalaayush@users.noreply.github.com>
2020-04-22 16:49:15 -0400

    Commented Out genrack method in Player.java

--- src/Player.java
+++ src/Player.java
@@ -40,43 +40,43 @@ public class Player extends Parent {
 //		ArrayList<LetterTilePic> currentletters;
 //	}
 	
-	/**
-	 * This method generates rack for the current player.
-	 * @param lb - the current letter bag
-	 */
-	public void genrack(LetterBag lb) {
-		DataFormat LetterTilePic = null;
-		rackletters = lb.getLetters(7);
-		for (LetterTilePicture letter : rackletters) {
-			getChildren().add(letter);
-			// letter.onMouseDragEnteredProperty();
-			letter.setOnDragDetected(new EventHandler<MouseEvent>() {
-				public void handle(MouseEvent event) {
-					/* drag was detected, start a drag-and-drop gesture */
-					/* allow any transfer mode */
-					Dragboard db = letter.startDragAndDrop(TransferMode.MOVE);
-					/* Put a string on a dragboard */
-					ClipboardContent content = new ClipboardContent();
-					// content.putString(letter.toString());
-					content.put(LetterTilePic, letter);
-					// content.getFiles();
-					// currentTile.getClass();
-					db.setContent(content);
-					event.consume();
-				}
-			});
-
-			letter.setOnDragDone(new EventHandler<DragEvent>() {
-				public void handle(DragEvent event) {
-					/* the drag and drop gesture ended */
-					/* if the data was successfully moved, clear it */
-					if (event.getTransferMode() == TransferMode.MOVE) {
-						getChildren().remove(letter);
-					}
-					event.consume();
-				}
-			});
-		}
-	}
+//	/**
+//	 * This method generates rack for the current player.
+//	 * @param lb - the current letter bag
+//	 */
+//	public void genrack(LetterBag lb) {
+//		DataFormat LetterTilePic = null;
+//		rackletters = lb.getLetters(7);
+//		for (LetterTilePicture letter : rackletters) {
+//			getChildren().add(letter);
+//			// letter.onMouseDragEnteredProperty();
+//			letter.setOnDragDetected(new EventHandler<MouseEvent>() {
+//				public void handle(MouseEvent event) {
+//					/* drag was detected, start a drag-and-drop gesture */
+//					/* allow any transfer mode */
+//					Dragboard db = letter.startDragAndDrop(TransferMode.MOVE);
+//					/* Put a string on a dragboard */
+//					ClipboardContent content = new ClipboardContent();
+//					// content.putString(letter.toString());
+//					content.put(LetterTilePic, letter);
+//					// content.getFiles();
+//					// currentTile.getClass();
+//					db.setContent(content);
+//					event.consume();
+//				}
+//			});
+//
+//			letter.setOnDragDone(new EventHandler<DragEvent>() {
+//				public void handle(DragEvent event) {
+//					/* the drag and drop gesture ended */
+//					/* if the data was successfully moved, clear it */
+//					if (event.getTransferMode() == TransferMode.MOVE) {
+//						getChildren().remove(letter);
+//					}
+//					event.consume();
+//				}
+//			});
+//		}
+//	}
 
 }
